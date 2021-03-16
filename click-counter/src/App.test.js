a5a408eecfdf8389d9ad92a9d9884fb300f1dda1
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-03-16 18:51:26 +0530

    Refactor App.test.js

--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -6,15 +6,27 @@ import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const setUp = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
 test("renders without error", () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setUp();
+  const appComponent = findByTestAttr(wrapper, "component-app");
   expect(appComponent).toHaveLength(1);
 });
 
-test("renders button", () => {});
+test("renders button", () => {
+  const wrapper = setUp();
+  const button = findByTestAttr(wrapper, "increment-button");
+  expect(button).toHaveLength(1);
+});
 
-test("renders counter display", () => {});
+test("renders counter display", () => {
+  const wrapper = setUp();
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
+  expect(counterDisplay).toHaveLength(1);
+});
 
 test("counter starts at 0", () => {});
 
