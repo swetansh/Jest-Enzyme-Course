33cde1f8a1bda2230de80fcf5bdc7476c25247c7
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-03-16 20:20:03 +0530

    Congrats Component TDD

--- jotto/src/Congrats.js
+++ jotto/src/Congrats.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Congrats = () => {
-  return null;
+const Congrats = (props) => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats" className="alert alert-success">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word.
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
 };
 
 export default Congrats;
--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -1,9 +1,26 @@
 import { shallow } from "enzyme";
 import React from "react";
+import { findByTestAttr } from "../test/testUtils";
 import Congrats from "./Congrats";
 
 const setup = (props = {}) => {
   return shallow(<Congrats {...props} />);
 };
 
-test("renders without error", () => {});
+test("renders without error", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-congrats");
+  expect(component).toHaveLength(1);
+});
+
+test("renders no text when `success` props is false", () => {
+  const wrapper = setup({ success: false });
+  const component = findByTestAttr(wrapper, "component-congrats");
+  expect(component.text()).toBe("");
+});
+
+test("renders non-empty congrats message when `success` prop is true", () => {
+  const wrapper = setup({ success: true });
+  const message = findByTestAttr(wrapper, "congrats-message");
+  expect(message.text().length).not.toBe(0);
+});
