9cc7eda2bd2d57e83a864c8bb95df6dd7f59d1ba
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-03-16 21:10:42 +0530

    Test PropTypes

--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -1,10 +1,13 @@
 import { shallow } from "enzyme";
 import React from "react";
-import { findByTestAttr } from "../test/testUtils";
+import { checkProps, findByTestAttr } from "../test/testUtils";
 import Congrats from "./Congrats";
 
+const defaultProps = { success: false };
+
 const setup = (props = {}) => {
-  return shallow(<Congrats {...props} />);
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
 };
 
 test("renders without error", () => {
@@ -23,4 +26,9 @@ test("renders non-empty congrats message when `success` prop is true", () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, "congrats-message");
   expect(message.text().length).not.toBe(0);
+});
+
+test("does not throw warning with expected props", () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
 });
