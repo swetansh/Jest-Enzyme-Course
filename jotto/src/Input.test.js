651b1ad422ed11a387aa9145e3671512ccf1d3a2
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-03-17 18:59:18 +0530

    Updated test for success = true

--- jotto/src/Input.test.js
+++ jotto/src/Input.test.js
@@ -33,9 +33,23 @@ describe("render", () => {
     });
 
     describe("word has been guessed", () => {
-      test("should render component without error", () => {});
-      test("should not render input box", () => {});
-      test("should not render submit button", () => {});
+      let wrapper;
+      beforeEach(() => {
+        const initialState = { success: true };
+        wrapper = setup(initialState);
+      });
+      test("should render component without error", () => {
+        const component = findByTestAttr(wrapper, "component-input");
+        expect(component).toHaveLength(1);
+      });
+      test("should not render input box", () => {
+        const inputBox = findByTestAttr(wrapper, "input-box");
+        expect(inputBox).toHaveLength(0);
+      });
+      test("should not render submit button", () => {
+        const submitButton = findByTestAttr(wrapper, "submit-button");
+        expect(submitButton).toHaveLength(0);
+      });
     });
   });
 });
