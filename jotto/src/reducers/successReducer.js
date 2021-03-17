252fb920abd83be487ffbd8d49bf2eb502aaee4a
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-03-17 17:24:35 +0530

    Add successReducer and Test

--- jotto/src/reducers/successReducer.js
+++ jotto/src/reducers/successReducer.js
@@ -1,6 +1,8 @@
+import { actionTypes } from "../actions";
+
 export default (state = false, action) => {
   switch (action.type) {
-    case "CORRECT_GUESS":
+    case actionTypes.CORRECT_GUESS:
       return true;
     default:
       return state;
