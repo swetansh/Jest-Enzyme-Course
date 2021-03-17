8d84b22e974162d228a728e5b1c5380924fa2f15
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-03-17 18:55:29 +0530

    add input component tdd

--- jotto/src/Input.test.js
+++ jotto/src/Input.test.js
@@ -14,9 +14,23 @@ const setup = (initialState = {}) => {
 
 describe("render", () => {
   describe("word has not been guessed", () => {
-    test("should render component without error", () => {});
-    test("should render input box", () => {});
-    test("should render submit button", () => {});
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
+    });
+    test("should render component without error", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component).toHaveLength(1);
+    });
+    test("should render input box", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox).toHaveLength(1);
+    });
+    test("should render submit button", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton).toHaveLength(1);
+    });
 
     describe("word has been guessed", () => {
       test("should render component without error", () => {});
--- jotto/src/configureStore.js
+++ jotto/src/configureStore.js
@@ -1,8 +1,7 @@
 import { createStore } from "redux";
-import ReduxThunk from "redux-thunk";
+
 import rootReducer from "./reducers";
 
-export const middlewares = [ReduxThunk];
 export default createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
