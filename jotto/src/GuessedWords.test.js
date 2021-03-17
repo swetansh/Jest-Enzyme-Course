595fe0f7e9c184d2331690e95838af5b50c1a6d4
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-03-17 14:29:21 +0530

    Failed Test Guess Word

--- jotto/src/GuessedWords.test.js
+++ jotto/src/GuessedWords.test.js
@@ -23,11 +23,39 @@ describe("if there no words guessed", () => {
   });
   test("renders without errors", () => {
     const component = findByTestAttr(wrapper, "component-guessed-words");
-    expect(component.length).toBe(1);
+    expect(component).toHaveLength(1);
   });
 
   test("renders intructions to guess a word", () => {
     const instructions = findByTestAttr(wrapper, "guess-instructions");
-    expect(instructions.text().length).not.toBe(0);
+    expect(instructions.text()).not.toHaveLength(0);
+  });
+});
+
+describe("if there are words guessed", () => {
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: "train", letterMatchCount: 3 },
+    { guessedWord: "agile", letterMatchCount: 1 },
+    { guessedWord: "party", letterMatchCount: 5 },
+  ];
+
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+
+  test("should render without error", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component).toHaveLength(1);
+  });
+
+  test("should render `guessed words` section", () => {
+    const guessedWordsNode = findByTestAttr(wrapper, "guessed-words");
+    expect(guessedWordsNode).toHaveLength(1);
+  });
+
+  test("should render correct number of guessed words", () => {
+    const guessedWordsNodes = findByTestAttr(wrapper, "guessed-word");
+    expect(guessedWordsNodes).toHaveLength(guessedWords.length);
   });
 });
