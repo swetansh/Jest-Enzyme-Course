29ce43d0c874f7bf87ed70615d12065c535d8e8b
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-03-18 14:09:29 +0530

    Setup Thunk Test

--- jotto/src/configureStore.js
+++ jotto/src/configureStore.js
@@ -1,8 +1,11 @@
-import { createStore } from "redux";
-
+import { applyMiddleware, createStore } from "redux";
+import ReduxThunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-export default createStore(
+export const middlewares = [ReduxThunk];
+const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
+
+export default createStoreWithMiddleware(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
--- jotto/test/testUtils.js
+++ jotto/test/testUtils.js
@@ -1,10 +1,12 @@
 import checkPropTypes from "check-prop-types";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import { middlewares } from "../src/configureStore";
 
 import rootReducer from "../src/reducers";
 
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
 
 export const findByTestAttr = (wrapper, val) => {
