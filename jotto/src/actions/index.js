24766d55e7770e7c56f3641c48661b0545d42e82
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-03-18 15:07:28 +0530

    Thunk Test complete

--- jotto/src/actions/index.js
+++ jotto/src/actions/index.js
@@ -1,3 +1,5 @@
+import { getLetterMatchCount } from "../helpers";
+
 export const actionTypes = {
   CORRECT_GUESS: "CORRECT_GUESS",
   GUESS_WORD: "GUESS_WORD",
@@ -9,6 +11,22 @@ export function correctGuess() {
   };
 }
 
-export const guessWord = (guessWord) => {
-  return function (dispatch, getState) {};
+export const guessWord = (guessedWord) => {
+  return function (dispatch, getState) {
+    const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: {
+        guessedWord,
+        letterMatchCount,
+      },
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch({
+        type: actionTypes.CORRECT_GUESS,
+      });
+    }
+  };
 };
--- jotto/src/reducers/guessedWordsReducer.js
+++ jotto/src/reducers/guessedWordsReducer.js
@@ -1,3 +1,10 @@
+import { actionTypes } from "../actions";
+
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case actionTypes.GUESS_WORD:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
 };
